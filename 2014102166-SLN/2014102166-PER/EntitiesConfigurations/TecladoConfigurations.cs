e8d6379e15fdd5f740a963866a37f119c4ddb0d2
Diego <diegoffr31@gmail.com>
2017-05-17 21:26:38 -0500

    Prueba 2

---
+++ 2014102166-SLN/2014102166-PER/EntitiesConfigurations/TecladoConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using _2014102166_ENT;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2014102166_PER.EntitiesConfigurations
+{
+    class TecladoConfigurations: EntityTypeConfiguration<Teclado>
+    {
+        public TecladoConfigurations()
+        {
+            //Relations Configurations
+            HasRequired(c => c.validarPin)
+                .WithRequiredPrincipal(c => c.Teclado);
+
+        }
+    }
+}
--- 2014102166-SLN/2014102166-PER/Lc2DbContext.cs
+++ 2014102166-SLN/2014102166-PER/Lc2DbContext.cs
@@ -1,4 +1,5 @@
 ﻿using _2014102166_ENT;
+using _2014102166_PER.EntitiesConfigurations;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -8,8 +9,23 @@ using System.Threading.Tasks;
 
 namespace _2014102166_PER
 {
-    class Lc2DbContext: DbContext
+    public class Lc2DbContext : DbContext
     {
         public DbSet<Cuenta> Cuentas { get; set; }
+        public DbSet<DispensadorEfectivo> Dispensadores { get; set; }
+        public DbSet<Pantalla> Pantallas { get; set; }
+        public DbSet<RanuraDeposito> Ranuras { get; set; }
+        public DbSet<Teclado> Teclados { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CuentaConfigurations());
+            modelBuilder.Configurations.Add(new DispensadorConfigurations());
+            modelBuilder.Configurations.Add(new PantallaConfigurations());
+            modelBuilder.Configurations.Add(new RanuraConfigurations());
+            modelBuilder.Configurations.Add(new TecladoConfigurations());
+            base.OnModelCreating(modelBuilder); 
+        }
     }
+
 }
